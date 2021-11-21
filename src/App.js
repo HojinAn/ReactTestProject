afda9c2cfd2e22cd82c558c1f289d7d385b691e2
HojinAn <asdv94@naver.com>
2021-11-21 23:51:05 +0900

    Practice AccessDom

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import { useState } from 'react'
 import Children from './Children'
 import './App.css';
+import AccessDom from './components/access_dom/AccessDom'
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
-  const onClickButton = (number) => {
-    setCount(count + number);
-  }
+  // const onClickButton = (number) => {
+  //   setCount(count + number);
+  // }
   return (
-    <div className="App">
-      <button onClick={() => onClickButton(+1)}>+1</button>
-      <button onClick={() => onClickButton(-1)}>-1</button>
-      <Children count={count} setCount={setCount}>
-        <Children count={count} setCount={setCount} />
-      </Children>
+    // <div className="App">
+    //   <button onClick={() => onClickButton(+1)}>+1</button>
+    //   <button onClick={() => onClickButton(-1)}>-1</button>
+    //   <Children count={count} setCount={setCount}>
+    //     <Children count={count} setCount={setCount} />
+    //   </Children>
+    // </div>
+    <div>
+      <AccessDom/>
     </div>
   );
 }
